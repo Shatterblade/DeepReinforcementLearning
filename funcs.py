c002cf82b3c82cdaa9096f292da73abf001480e1
shatterblade <ethan@ethans-macbook.attlocal.net>
2018-10-08 21:12:26 -0700

    Maybe now?

--- funcs.py
+++ funcs.py
@@ -81,6 +81,8 @@ def playMatches(player1, player2, EPISODES, logger, turns_until_tau0, memory = N
 
         while done == 0:
             turn = turn + 1
+            if printmoves:
+                env.gameState.print()
     
             #### Run the MCTS algo and return an action
             if turn < turns_until_tau0:
@@ -104,7 +106,6 @@ def playMatches(player1, player2, EPISODES, logger, turns_until_tau0, memory = N
             state, value, done, _ = env.step(action) #the value of the newState from the POV of the new playerTurn i.e. -1 if the previous player played a winning move
             
             env.gameState.render(logger)
-            env.gameState.print()
 
             if done == 1: 
                 if memory != None:
